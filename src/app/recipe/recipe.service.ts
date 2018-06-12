af09a4e972b147e0c97d9d02f098ad9a8fdf355c
Gabriel Radureau <gradureau@excilys.com>
2018-06-12 14:35:12 +0200

    get cocktail recipes from server

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Recipe } from './recipe.model';
-import { RECIPES } from './recipes.mock';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor() { }
+  private _baseUrl = 'http://10.0.1.55:8080/api/v1/recipes';
+
+  constructor(private httpClient: HttpClient) { }
 
   getRecipes(): Observable<Recipe[]> {
-    return of(RECIPES);
+    return this.httpClient.get<Recipe[]>(this._baseUrl);
   }
 }
